6a346942e91362b6e57ba56b7cc355e33cae2031
Henrik <henrik.saega@gmail.com>
2026-05-28 12:58:52 +0300

    hotfix

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -68,6 +68,8 @@ export default function SignInScreen() {
 const styles = StyleSheet.create({
   background: {
     flex: 1,
+    width: "100%",
+    height: "100%",
   },
   overlay: {
     ...StyleSheet.absoluteFillObject,
